151302b9c6ada91b74e40b7d03c0d1e303d1821e
OrangeGeng <chengsefengbaogeng@126.com>
2013-09-11 16:37:41 +0800

    添加一个简单Activity基类
    同时还整了整资源

---
+++ src/com/buaa/buaaers/common/BaseSimpleActivity.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.buaa.buaaers.common;
+
+import com.buaa.buaaers.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * 用在次级界面
+ * @author gordongeng
+ *
+ */
+public class BaseSimpleActivity extends Activity {
+
+    protected LinearLayout mContainer;
+    
+    protected ImageView mTitle, mTopBarLogo, mRightBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_simple_layout);       
+        
+        mContainer = (LinearLayout)findViewById(R.id.container);
+        
+        mTopBarLogo = (ImageView)findViewById(R.id.logo_image);
+        mRightBtn = (ImageView)findViewById(R.id.top_bar_right_btn);
+        mTitle = (ImageView)findViewById(R.id.topbar_title);
+    }
+}
