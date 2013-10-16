d8db72d8c98f719b1a2e66bc7fbde1b855085e8c
OrangeGeng <chengsefengbaogeng@126.com>
2013-10-16 10:46:02 +0800

    调整登陆界面
    更改布局+增加资源文件

--- src/com/buaa/buaaers/LoginActivity.java
+++ src/com/buaa/buaaers/LoginActivity.java
@@ -5,28 +5,26 @@ import com.buaa.buaaers.college.CollegeActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends Activity implements OnClickListener{
 
-	private Button mLoginBtn;
+	private ImageView mLoginBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	    requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
-		mLoginBtn = (Button)findViewById(R.id.loginBtn);
+		mLoginBtn = (ImageView)findViewById(R.id.loginBtn);
 		mLoginBtn.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View v) {
 		if (mLoginBtn == v) {
 			Intent intent = new Intent(this, CollegeActivity.class);
-			Log.e("gordongeng", "this one");
             startActivity(intent);
 		}
 		
